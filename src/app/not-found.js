67ca707688485ce34746e7d6c50cbf4f99e92033
okuhlecharlieman <114597427+okuhlecharlieman@users.noreply.github.com>
2025-07-24 10:43:15 +0200

    Create not-found.js

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+// src/app/not-found.js
+export default function NotFound() {
+  return (
+    <div className="text-center p-4">
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
