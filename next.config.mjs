84d2840c8e6d82e02117fc097272ef8947b2f352
okuhlecharlieman <114597427+okuhlecharlieman@users.noreply.github.com>
2025-07-28 12:55:44 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,16 +8,6 @@ const nextConfig = {
   experimental: {
     webpackBuildWorker: true,
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(js|mjs)$/,
-      include: /node_modules\/undici/,
-      use: {
-        loader: 'next-swc-loader',
-      },
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
